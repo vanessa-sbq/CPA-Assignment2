7157708ca26bc75e9d9eeb062845ab8c45d30d09
Duarte Souto Assunção <up202208319@up.pt>
2026-05-16 17:16:49 +0100

    Block algorithm for LU factorization

--- src/lu_fact.cpp
+++ src/lu_fact.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <omp.h>
 #include "lu_fact.hpp"
@@ -22,9 +23,42 @@ auto lufact::sequential(Matrix<double>& A) -> void {
 }
 
 auto lufact::block(Matrix<double>& A, unsigned block_size) -> void {
-    (void) A;
-    (void)block_size;
-    // TODO
+    for (unsigned k = 0; k < A.size - 1; k++) {
+        if (A(k, k) != 0) {
+            std::cerr << "Error: value 0 found in matrix diagonal. Aborting..." << std::endl;
+            std::exit(1);
+        }
+
+        // Compute column k:
+        unsigned vertical_blocks = (A.size + block_size - 1) / block_size; // Equivalent to roundup(A.size / block_size)
+        for (unsigned b = 0; b < vertical_blocks; b++) { // For each block
+            for ( // i is the line where the block starts (cannot be < k+1 neither >= A.size)
+                unsigned i = std::max(k+1, b * block_size);
+                i < std::min(A.size, (b+1) * block_size);
+                i++
+            ) {
+                A(i, k) /= A(k, k);
+            }
+        }
+
+        // Compute matrix A[k+1..size, k+1..size]:
+        unsigned n_blocks = vertical_blocks * vertical_blocks;
+        for (unsigned b = 0; b < n_blocks; b++) { // For each block
+            for ( // i is the line where the block starts (cannot be < k+1 neither >= A.size)
+                unsigned i = std::max(k+1, b * block_size);
+                i < std::min(A.size, (b+1) * block_size);
+                i++
+            ) {
+                for ( // j is the column where the block starts (cannot be < k+1 neither >= A.size)
+                    unsigned j = std::max(k+1, b * block_size);
+                    j < std::min(A.size, (b+1) * block_size);
+                    j++
+                ) {
+                    A(i, j) -= A(i, k) * A(k, j);
+                }
+            }
+        }
+    }
 }
 
 auto lufact::omp(Matrix<double>& A, unsigned num_threads) -> void {
